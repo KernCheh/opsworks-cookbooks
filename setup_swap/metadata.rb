44adfd3336692652898bb1ff96f03cd3ca0c1ae4
Sim Kern Cheh <kern.cheh@gmail.com>
2016-06-06 00:03:43 +0800

    Setup swap recipe

---
+++ setup_swap/metadata.rb
@@ -0,0 +1,9 @@
+name 'setup_swap'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures setup_swap'
+long_description 'Installs/Configures setup_swap'
+version '0.1.0'
+
+depends 'opsworks_initial_setup'
