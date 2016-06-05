056a4465a855a5ef66d4896cd5d3ef4e9ff5b5e7
Sim Kern Cheh <kern.cheh@gmail.com>
2016-06-06 00:57:21 +0800

    Set java home recipe

---
+++ set_java_home/metadata.rb
@@ -0,0 +1,9 @@
+name 'set_java_home'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures set_java_home'
+long_description 'Installs/Configures set_java_home'
+version '0.1.0'
+
+recipe 'set_java_home::setup', 'Set up JAVA_HOME in /etc/environment'
